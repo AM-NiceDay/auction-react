ea9b294578c6e3f4cf3259069f48dd4a667b0ed6
Aliaksei Krasikau <a.krasikau@gmail.com>
2015-12-14 23:57:16 +0300

    Add websockets support. Send actions throw websockets

--- src/actions/room.js
+++ src/actions/room.js
@@ -4,13 +4,19 @@ export const JOIN_ROOM = 'JOIN_ROOM';
 export function createRoom(owner) {
   return {
     type: CREATE_ROOM,
-    owner
+    owner,
+    meta: {
+      remote: true
+    }
   }
 }
 
 export function joinRoom(player) {
   return {
     type: JOIN_ROOM,
-    player
+    player,
+    meta: {
+      remote: true
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import Index from './components/Index';
 import Room from './components/Room';
 import reducers from './reducers';
+import io from 'socket.io-client';
+import remoteActionMiddleware from './middlewares/remoteActionMiddleware';
 
-const store = createStore(reducers);
+var socket = io('http://localhost:8000/');
 
+const createStoreWithMiddleware = applyMiddleware(remoteActionMiddleware(socket))(createStore);
+const store = createStoreWithMiddleware(reducers);
 
 console.log(store.getState().toJS());
 store.subscribe(() => {
